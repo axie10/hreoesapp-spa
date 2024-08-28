bf25972d76d48194c1bfb487ba4d6be488e162d6
axie10 <cryptosushi21@gmail.com>
2024-08-28 18:31:28 +0200

    creamos directorios del proyecto e introduzco el navbar

---
+++ src/HeroesApp.jsx
@@ -0,0 +1,18 @@
+
+import Button from '@mui/material/Button';
+import PrimarySearchAppBar from './shared/components/NavBar';
+
+export const HeroesApp = () => {
+
+
+    return (
+
+        <>
+            <PrimarySearchAppBar/>
+            {/* <h1>HeroesApp</h1>
+
+            <Button variant="contained">Ir a heore</Button> */}
+        </>
+
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import './style.css'
+import { HeroesApp } from './HeroesApp'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <HeroesApp />
   </StrictMode>,
 )
